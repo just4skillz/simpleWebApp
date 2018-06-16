a9c078a2f65e6570f4f36580745da37dc361f3fa
Octavian Hera <remus.hera@gmail.com>
2018-06-16 21:07:26 +0300

    Add capabilities to write tests

---
+++ src/test/java/TestWebApp.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class TestWebApp {
+
+    private WebDriver driver;
+
+    @BeforeClass
+    public void beforeClass() {
+        System.setProperty("webdriver.gecko.driver", "src/test/resources/driver/geckodriver.exe");
+        driver = new FirefoxDriver();
+    }
+
+    @Test
+    public void firstTest() throws InterruptedException {
+        driver.get("http://localhost:9090/webapp/index.html");
+//        driver.get("https://www.google.ro/");
+        Thread.sleep(5000);
+
+        driver.quit();
+    }
+
+}
